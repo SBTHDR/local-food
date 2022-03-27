66bef0edc9b4cbcb32d7bdbe3c32f8a9ba3572bf
SBTHDR <subrata.halder13d@gmail.com>
2022-03-27 20:42:57 +0600

    Admin Seeder

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Kimberley Heath',
+            'email' => 'kimberley@mailinator.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('Pa$$w0rd!'),
+            'remember_token' => Str::random(10),
+            'is_admin' => true
+        ]);
+    }
+}
